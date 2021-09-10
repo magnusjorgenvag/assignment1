581a7d82b61a242d2f2eebc36a96a451c793ccbb
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2021-09-10 13:56:33 +0200

    Add splitline method (#3)
    Co-authored-by: Thor Tudal Lauridsen <thtl@itu.dk>

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,7 +8,14 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach(var str in lines) {
+                var output = Regex.Split(str, "[^a-zA-Z0-9\\d\\s:]");
+                foreach(var str2 in output) {
+                    if (!String.IsNullOrEmpty(str2)) {
+                        yield return str2;
+                    }
+                }
+            }
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
