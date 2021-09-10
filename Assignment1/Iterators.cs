6c9484f390cdde5dec5fa94ede31693d57ff0bc8
Thor Tudal Lauridsen <thtl@itu.dk>
2021-09-10 12:15:11 +0200

    First commit

---
+++ Assignment1/Iterators.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+    public static class Iterators
+    {
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            Console.WriteLine("hej");
+
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
